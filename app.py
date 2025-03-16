a669024604e8fc71cab0f5338a402694a1c55849
Sachin S <tmsachins@gmail.com>
2025-03-16 13:52:07 -0500

    created app file

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = tf.keras.models.load_model('model.h5')
